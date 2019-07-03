a5965fdb1fff0b4cfd28342bb399afbbf60f7aaf
BeGreaterLipan <44823819+BeGreaterLipan@users.noreply.github.com>
2019-07-04 01:15:18 +0800

    Add files via upload
    Morse.h文件选错了

--- Morse.h
+++ Morse.h
@@ -6,7 +6,11 @@ class Morse
     Morse(int pin);
     void dot();
     void dash();
+    void c_space();
+    void w_space();
+    void to_Morse(char input, Morse n);
   private:
     int _pin;
+    int _dottime;
 };
 #endif /*_MORSE_H*/
